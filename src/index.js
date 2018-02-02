6e1a05859e9b8d010f19795daf1c94819cf7daf8
Patrick Miner <patrickminer318@gmail.com>
2018-02-01 19:14:17 -0800

    Changed App to class component. Performs Youtube search upon rendering

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
 import React from 'react';
+import { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
-
-
 
 const API_KEY = 'AIzaSyCD2ygjUk6ZV5UjutyDaSOOOgEzfJa-L24';
 
 // Create a new component. This component should create HTML
 // This is done with the standard React library
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos, err) => {
+      if (err) {
+        throw err;
+      } else {
+        this.setState({videos})
+        console.log(this.state.videos);
+      }
+    });
+
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
 
 // Take created HTML and put it on the page (in the DOM)
