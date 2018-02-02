1af5e92c6f2f989869baa236c9ac16098ca00b32
Patrick Miner <patrickminer318@gmail.com>
2018-02-01 17:41:39 -0800

    Updated searchbar to change component state on change

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 // import ReactDOM from 'react-dom';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+  
   render() {
     return (
-      // onChange is built into HTML and is part of javascript
-      <input onChange={(event) => { console.log(event.target.value) }} />
+      // onChange is built into HTML and is part of javascript, so is 'event.target.value'
+      <div>
+        <input onChange={(event) => {this.setState({term: event.target.value})}} />
+      </div>
     );
   }
 
