c77e85a180affd552cfd89b1a64018b5fd1b42f1
Patrick Miner <patrickminer318@gmail.com>
2018-02-09 13:51:37 -0800

    Created VideoDetail component to display selected video

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCD2ygjUk6ZV5UjutyDaSOOOgEzfJa-L24';
 
@@ -30,6 +31,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail />
         <VideoList videos={this.state.videos}/>
       </div>
     )
